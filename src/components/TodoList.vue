3c5fe566b5e5a54df60172414ed1f936d526f2cb
Vuejs <vuejs@ZiMacPro-6.local>
2018-11-24 14:42:15 +0100

    added v-model todo.completed to li

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,37 +1,47 @@
 <template>
   <div class="todo-list">
+
     <form class="addbox-wr" @submit.prevent="add">
-      <input type="text" name="addbox" v-model.trim="addBox" @keyup.enter="add">
+      <input type="text" name="addbox" v-model.trim="addBox">
       <button>Add Task</button>
     </form>
+
     <div v-if="todoList.length">
       <div v-if="incompleted.length" class="added-tasks">
+        
         <h3>Pending tasks ({{incompleted.length}})</h3>
         <ul>
+          
           <li v-for="(todo, index) in todoList" :key="index" :class="{hide:todo.completed == true}">
             <p>
-              <input type="checkbox" @change="complete(todo)">{{ todo.item }} <button @click="remove(todo)" :disabled="todo.completed == true">Delete</button>
+              <input type="checkbox" v-model="todo.completed">{{ todo.item }} <button @click="remove(todo)" :disabled="todo.completed == true">Delete</button>
               </p>
           </li>
+
         </ul>
+
       </div>
       <div v-else>
         <p>All tasks completed!</p>
       </div>
       <div v-if="completed.length" class="completed-tasks">
         <div class="added-tasks">
+          
           <h3>Completed tasks ({{completed.length}})</h3>
           <ul>
             <li v-for="(todo, index) in completed" :key="index" :class="{done:todo.completed == true}">
               <p>{{ todo.item }} <button @click="undo(todo)">Undo</button></p>
             </li>
           </ul>
+
         </div>
       </div>
     </div>
+
     <div v-else>
       <p>There are no tasks yet</p>
     </div>
+
   </div>
 </template>
 
@@ -61,9 +71,6 @@ export default {
     remove: function(todo) {
       var index = this.todoList.indexOf(todo);
       this.todoList.splice(index, 1);
-    },
-    complete: function(todo){
-      todo.completed = !todo.completed;
     },
     undo: function(todo){
       todo.completed = !todo.completed;
